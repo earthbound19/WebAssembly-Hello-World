eda74c972491ea64cefb82cf983affb3a15c5992
Alex Hall <artdev@inboxalias.com>
2017-03-21 23:14:42 -0600

    Initial commit of all project files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# WebAssembly-Hello-World
-Very basic transpile sources from c and cpp targeting WebAssembly end compilation.
+# WebAssembly Hello World
+
+At this writing, this repository contains two (extremely) simple (as far as programming goes, which is all complex on some level) c and cpp sources (let's insert another interrupting parenthetical here!) which may be transpiled to JavaScript .asm and further to WebAssembly .wasm files (with supporting .js hooks and .html files to load them), and run directly by opening the HTML files in supporting browsers (I am aware of only firefox working at this writing) (let's throw in a sixth parenthetical just for kicks and giggles!).
+
+## Installation and usage
+
+See the comments in webAssemble.sh and the lines of code in the file to set up a Web Assembly transpile environment and to transpile the c and cpp sources.
+
+# LICENSE
+
+Whatever here can't be construed as Public Domain already (super simple, fundamental examples), I wrote and release into the Public Domain.
---
+++ webAssemble.sh
@@ -0,0 +1,17 @@
+# NOTE that you must first have an sdk environment; the following lines of code, uncommented, will get you that before running the transpile of .c and .cpp sources to WebAssembly, assuming you've got a c++ compiler environment in your PATH; see: http://webassembly.org/getting-started/developers-guide/
+
+# git clone https://github.com/juj/emsdk.git
+# cd emsdk
+# ./emsdk install sdk-incoming-64bit binaryen-master-64bit
+# ./emsdk activate sdk-incoming-64bit binaryen-master-64bit
+# source ./emsdk_env.sh
+# cd ..
+
+emcc ./helloworld.c -s WASM=1 -o helloworld.html
+
+emcc ./cin_cout_test.cpp -s WASM=1 -o cin_cout_to_wasm_test.html
+
+# ADDITIONAL NOTES; at this writing:
+# - the compiler toolkit automatically adds some header/front material fluff. That can be removed; how I don't know yet.
+# - I am able to run the web assembly by simply opening the accompanying .html files from local storage (a file on my mac) in firefox. Safari chokes on it, though. What? Yer supposed to be "innovate," you Mac folks ;)
+# - the cin_cout_test.cpp doesn't behave as would be hoped; in firefox at least it deplays and then vanishes a message (way too fast probably even to be actually rendered), followed by a mysterious javascript(-esque?) input prompt.
